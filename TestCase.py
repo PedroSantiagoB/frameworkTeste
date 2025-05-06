6b780a5e3de09d9256f968797dfbbad47b701896
PedroSantiagoB <pedrogomespego@gmail.com>
2025-05-05 23:53:26 -0300

    4. Testando TestCase

--- TestCase.py
+++ TestCase.py
@@ -44,3 +44,108 @@ class TestResult:
         return f'{self.run_count} {self.RUN_MSG}, ' \
                f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
                f'{str(len(self.errors))} {self.ERROR_MSG}'
+    
+class MyTest(TestCase):
+
+    def set_up(self):
+        print('set_up')
+
+    def tear_down(self):
+        print('tear_down')
+
+    def test_a(self):
+        print('test_a')
+
+    def test_b(self):
+        print('test_b')
+
+    def test_c(self):
+        print('test_c')
+
+result = TestResult()
+
+test = MyTest('test_a')
+test.run(result)
+
+test = MyTest('test_b')
+test.run(result)
+
+test = MyTest('test_c')
+test.run(result)
+
+print(result.summary())
+
+class TestStub(TestCase):
+
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
+
+
+class TestSpy(TestCase):
+
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
+
+class TestCaseTest(TestCase):
+
+    def set_up(self):
+        self.result = TestResult()
+
+    ...
+
+    def test_was_set_up(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_set_up
+
+    def test_was_run(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_run
+
+    def test_was_tear_down(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_tear_down
+
+    def test_template_method(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.log == "set_up test_method tear_down"
+
+
+test = TestCaseTest('test_was_set_up')
+test.run(result)
+
+test = TestCaseTest('test_was_run')
+test.run(result)
+
+test = TestCaseTest('test_was_tear_down')
+test.run(result)
+
+test = TestCaseTest('test_template_method')
+test.run(result)
+
+print(result.summary())
