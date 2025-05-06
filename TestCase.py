84aea8095a183f38acb75d06a6bcf9190d2db5c6
PedroSantiagoB <pedrogomespego@gmail.com>
2025-05-05 23:58:15 -0300

    7. Executando Todos os Testes

--- TestCase.py
+++ TestCase.py
@@ -269,3 +269,16 @@ suite = loader.make_suite(TestLoaderTest)
 
 runner = TestRunner()
 runner.run(suite)
+
+loader = TestLoader()
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
+
+runner = TestRunner()
+runner.run(suite)
